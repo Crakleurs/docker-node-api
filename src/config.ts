60aa8eb6914a56eaeb65f51c7583e49319751735
Crakleurs <eti.faviere@gmail.com>
2023-01-04 12:36:31 +0000

    add docker

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ export const config = {
   API_PORT: 8080,
   PRODUCTION: false,
   database: {
-    USERNAME: "root",
-    PASSWORD: "root",
-    DATABASE: "root",
+    USERNAME: "docker_user",
+    PASSWORD: "d3kTrklm54208",
+    DATABASE: "docker",
     PORT: 5432,
     HOST: "localhost"
   }

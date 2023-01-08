f046688bb31c3881b95e89ddc4c462de941ec808
efaviere <eti.faviere@gmail.com>
2023-01-08 17:14:59 +0100

    fix

--- src/resources/containers/containers.service.ts
+++ src/resources/containers/containers.service.ts
@@ -13,7 +13,7 @@ export class ContainersService {
 
   async countRunning() {
     const docker = new Docker();
-    const containers = await docker.listContainers({all: false});
+    const containers = await docker.listContainers({filter: {status: "running"}});
     return containers.length;
   }
 

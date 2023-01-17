c03ec8afead77f774eac9842618053c7d1ffdd01
efaviere <eti.faviere@gmail.com>
2023-01-17 19:36:15 +0100

    add secure env vars

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ export const config = {
   API_PORT: 8080,
   PRODUCTION: false,
   database: {
-    USERNAME: "docker_user",
-    PASSWORD: "d3kTrklm54208",
-    DATABASE: "docker",
+    USERNAME: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "password",
+    DATABASE: process.env.DB_NAME || "db",
     PORT: 5432,
     HOST: process.env.NODE_ENV == "docker" ? "docker-app-db" : "localhost"
   },
-  jwtSecret: "RUqz!Nwo4Q6t4&Im^2@54DZ@l6T",
+  jwtSecret:  process.env.JWT_SECRET || "RUqz!Nwo4Q6t4&Im^2@54DZ@l6T",
   saltRounds: 10
 }
 

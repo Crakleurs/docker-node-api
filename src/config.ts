d0250da32b02edc9b4b0f00d6b3311b5a160365f
efaviere <eti.faviere@gmail.com>
2023-01-18 12:12:29 +0100

    add host properties to docker-compose

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,8 @@ export const config = {
     USERNAME: process.env.DB_USER || "root",
     PASSWORD: process.env.DB_PASSWORD || "password",
     DATABASE: process.env.DB_NAME || "db",
-    PORT: 5432,
-    HOST: process.env.NODE_ENV == "docker" ? "docker-app-db" : "localhost"
+    PORT: process.env.DB_PORT || 5432,
+    HOST: process.env.DB_HOST || "localhost"
   },
   jwtSecret:  process.env.JWT_SECRET || "RUqz!Nwo4Q6t4&Im^2@54DZ@l6T",
   saltRounds: 10
